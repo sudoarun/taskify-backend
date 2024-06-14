b7e4f9111b0305b6d94ac77dbf5f689a9a27dbd2
sudoarun <arunrajorya1999@gmail.com>
2024-06-14 11:55:11 +0530

    install donenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/taskifyRoutes");
+const env = require("dotenv");
+env.config();
 // PORT of the app
 const PORT = process.env.PORT || 5500;
 const URI = process.env.URI || "mongodb://127.0.0.1:27017/taskify";
@@ -27,5 +29,5 @@ app.use((req, res) => {
 // Listening on Port
 
 app.listen(PORT, () => {
-  console.log("server started...");
+  console.log(`Server started on Port ${PORT}`);
 });
